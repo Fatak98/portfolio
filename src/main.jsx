e2caf593754bf3240f222f6ec9c8fcda4b7272b9
Fatak <s300130@studenti.polito.it>
2026-04-30 19:10:40 +0200

    fix bootstrap css load

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
-import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App.jsx";
 import "./index.css";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter >
+    <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>
