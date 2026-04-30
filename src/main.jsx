ab4e19dd2ec202fff198ec6371b179e2bae13fa0
Fatak <s300130@studenti.polito.it>
2026-04-30 18:59:35 +0200

    fix github pages basename

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import { HashRouter  } from 'react-router-dom';
-createRoot(document.getElementById('root')).render(
-  <HashRouter>
-    <App />
-  </HashRouter >,
-)
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/portfolio">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
