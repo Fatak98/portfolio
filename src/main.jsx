08c864d686505bd905cffc628bd2846a0d2401f5
Fatak <s300130@studenti.polito.it>
2026-04-30 18:55:17 +0200

    update

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,9 @@ import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter  } from 'react-router-dom';
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter >,
 )
